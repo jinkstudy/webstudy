f46de3fdaf4520b0518d183e877c94374f8abcb7
jinkstudy <sweetyks486@gmail.com>
2019-06-03 18:19:46 +0900

    no message

---
+++ WebUI/WebContent/4_javascript_class/99_practice/2_menupan/script.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+window.onload = function (){
+
+	var price = [1000,2000,3000,2000,1000,1500];
+	var content = ['아메리카노','카페라떼','쥬스','과자','엿','땅콩'];
+
+	//#### 1. TD에 이벤트 발생
+	
+		window.onload = function(){
+	
+	var btn = document.getElementById('btn');
+	btn.onclick = function(){
+		alert('이벤트방식2')
+	}
+} 
+	
+	
+	//#####  2. 확인 버튼 클릭시 선택한 TD의 색을 다시 흰색으로
+	//#####  3. 값 계산하여 총합 구하기 - 각각 TD 클릭시 배열에 저장하였다가 계산
+
+	
+	
+}
