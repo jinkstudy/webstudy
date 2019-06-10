c38d8f832ccd2b94ad0a0c165dfab6f4975291ca
jinkstudy <sweetyks486@gmail.com>
2019-06-10 09:51:12 +0900

    no message

--- WebUI/WebContent/4_javascript_class/05_form/js/formCheck.js
+++ WebUI/WebContent/4_javascript_class/05_form/js/formCheck.js
@@ -1,8 +1,34 @@
 window.onload = function(){
 
-		var frm = document.getElementById('frm');
-		var inputs = document.querySelectorAll("input");
+//		var frm = document.getElementById('frm');
+//		var inputs = document.querySelectorAll("input");
 		
+	var frm = document.querySelector('#frm');
+	var inputs = document.querySelectorAll("input");
+		
+//	frm.onsubmit = function(e){
+		frm.onclick = function(e){
+//			e.preventDefault();
+//			e.stopPropagation();
+			var agree = frm.agree;
+			//alert(agree.checked);
+			if(!agree.checked){
+				alert('반드시 확인하셔야 합니다.')
+				return;
+			}
+		var nick= ['babo','geni','idiot','gdb'];
+		
+				if(!nick.includes(frm.nickname.value)){
+					alert('다시!!!')
+					return;
+				}
+		
+			
+			
+			
+			
+			frm.submit();
+		}
 
 
 	}
