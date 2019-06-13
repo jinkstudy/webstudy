6715939f0e6d9a85f89a60d7a022062c7be109ca
jinkstudy <sweetyks486@gmail.com>
2019-06-13 16:41:54 +0900

    no message

--- WebUI/WebContent/5_jquery_class/z_doit_project_class/js/jquery.2.js
+++ WebUI/WebContent/5_jquery_class/z_doit_project_class/js/jquery.2.js
@@ -3,8 +3,55 @@
 
 $(function(){
 	// 날짜 표시.
+	var today = new Date();
 	
-	$('#date_wrap .year' ).text('2019');
+	$('#date_wrap .year' ).text(today.getFullYear());
+	$('#date_wrap .month' ).text(today.getMonth()+1);
+	$('#date_wrap .date' ).text(today.getDate());
+	
+	// 검색창 커서 입력 시 그림 숨기기
+
+	
+	$('#keyword').focus(function(){
+		//alert("체크");
+		
+			$('#keyword').css('background', 'white');
+	
+	});
+	$('#keyword').blur(function(){
+		
+		$('#keyword').css('background', 'url("../../images/sch_guide.gif")');
+	});
+	
+	// 탭메뉴
+		var tab = $('#tabmenu dt').find('a');
+		var list =$('#tabmenu dd ul').find('li') ;
+		
+		list.hide();
+		
+		var lasttab = $('.tab_btn1');
+		var alt = lasttab.find('img').attr('alt');
+		
+		
+		//탭 클릭 시 ,
+		tab.click(function(){
+			//클릭 된 탭의 alt 값을 읽어온다.
+			//alert(alt);
+			
+			alt = lasttab.find('img').attr('alt');
+			var lastlist = $('li:contains("'+alt+'")');
+			//var lastlist = $('"'+text+':contains("'+alt+'")').parent();
+			alert('li:contains("'+alt+'")');
+			lastlist.show();
+			
+		 
+//		
+//		 alert(text);
+		// alert('text:contains("'+alt+'")');
+	
+	});
+		
+
 	
 	
 	//로그인 버튼 클릭 시
